aae660fb8dd7298a8446c928a4ee61ab6eac9f30
Github1234fab <fabien.marceau@gmail.com>
2024-07-01 08:33:27 +0200

    reste à faire l'ombre sur les boutons au click

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 //récupération des éléments du DOM
+//récupération des boutons  de choix d'opération
+let operationButtonsChoice = document.querySelectorAll(".button-choice-operation");
 //récupération de l'input user
 // let numberChosenByUser = document.querySelector(".input-choice");
-let numberChosenByUser = document.querySelectorAll(".button-choice");
+let numberChosenByUser = document.querySelectorAll(".button-choice-table");
 //récupération du button submit
 let button = document.querySelector(".button");
 //récupération de la basile P pour afficher les résultats
@@ -15,16 +17,30 @@ numberChosenByUser.forEach((el) => {
         el.addEventListener("click", (e) => {
                 console.log(e.target);
                 numberValueChosenByUser = parseFloat(e.target.value);
+                el.style.boxShadow = "0 0 10px 1px black inset";
+        });
+});
+
+
+let operationButtonsValueChoice;
+
+//écoute de l'événement click sur les buttons de choix d'opération
+operationButtonsChoice.forEach((el) => {
+        el.addEventListener("click", (e) => {
+                console.log("operation");
+                console.log(e.target);
+                operationButtonsValueChoice = e.target.value;
+                console.log(operationButtonsValueChoice);
         });
 });
 
 //écouter l'événement click sur le button submit
-
 button.addEventListener("click", () => {
+        console.log("submit");
         //stockage des symboles d'opérations
         let symbole;
         //stockage de la valeur de l'input
-        let optionChosenByUser = operationOptions.value;
+        // let optionChosenByUser = operationOptions.value;
         //  array pour stocker les opérations dans la boucle for.
         let arrayOfResultat = [];
 
@@ -32,38 +48,36 @@ button.addEventListener("click", () => {
         for (let i = 0; i <= 10; i++) {
                 //pour stocker le résultat des opérations
                 let result;
-
-                switch (optionChosenByUser) {
-                        case "multiplication":
+                switch (operationButtonsValueChoice) {
+                        case "x":
                                 result = numberValueChosenByUser * i;
                                 console.log(result);
                                 symbole = "x";
                                 arrayOfResultat.push(`${numberValueChosenByUser} ${symbole} ${i} = ${result}`);
                                 break;
-                        case "addition":
+                        case "+":
                                 result = numberValueChosenByUser + i;
                                 symbole = "+";
                                 arrayOfResultat.push(`${numberValueChosenByUser} ${symbole} ${i} = ${result}`);
                                 break;
-                        case "division":
+                        case "/":
                                 result = (numberValueChosenByUser / numberValueChosenByUser) * i;
                                 console.log(result);
                                 symbole = "/";
                                 arrayOfResultat.push(`${numberValueChosenByUser * i} ${symbole} ${numberValueChosenByUser} = ${result}`);
                                 break;
-                        case "soustraction":
+                        case "-":
                                 result = i + numberValueChosenByUser - numberValueChosenByUser;
                                 symbole = "-";
                                 arrayOfResultat.push(`${i + numberValueChosenByUser} ${symbole} ${numberValueChosenByUser} = ${result}`);
                                 break;
                 }
         }
-
         displayOperationResult.innerHTML = "";
         arrayOfResultat.forEach((el) => {
                 let displayResult = document.createElement("li");
                 displayResult.textContent = el;
-                displayResult.style.display = "flex";
+                displayResult.style.fontSize = "0.8rem";
                 displayOperationResult.appendChild(displayResult);
         });
 });
